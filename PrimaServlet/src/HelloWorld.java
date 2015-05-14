367f729ed106d84d81a7080d0699e758a9c52526
simoncini <nsimoncini@icloud.com>
2015-05-14 19:56:05 +0200

    Inserimento Primo Progetto prova

---
+++ PrimaServlet/src/HelloWorld.java
@@ -0,0 +1,40 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(
+		name = "HelloWorld",
+		description = "Servlet di test",
+		urlPatterns = "/hello"
+		)
+
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		System.out.println("HelloWorld Servlet inizializzata!");
+	}
+
+
+    public HelloWorld() {
+        // TODO Auto-generated constructor stub
+    }
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		this.getServletContext().log("Messaggio di log prima prova");
+		response.setContentType("text/plain");
+		response.getOutputStream().write("Benvenuto!".getBytes());
+		
+	}
+
+}
